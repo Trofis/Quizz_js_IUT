3e209b200b1b7802ce235fa02665f8bbe0126fbc
Trofis <porto.thomas@gmail.com>
2017-05-18 09:43:40 +0200

    Button modif/supp --> worked & modif in progress - Link to quizz .../Quizz.html/id

--- js/home.js
+++ js/home.js
@@ -24,23 +24,64 @@ $(function() {
         function remplirTaches(tasks)
         {
           $('#quizz_list').empty();
-          var url = window.location.href;
+          $("#liste_button").empty();
+          var url = document.domain;
           for(var i=0;i<tasks.length;i++){
               console.log(tasks[i]);
               $('#quizz_list')
               .append($('<li class="list-group-item">')
-              .append($('<a href='+url+tasks[i]["id"]+'>')
+              .append($('<a href='+url+"Quizz.html/"+tasks[i]["id"]+'>')
                   .text(tasks[i].title)
                   ).on("click", tasks[i], goTo)
               );
+
+              $("#liste_button")
+              .append($("<div class='btn-group ul_btn' role='group'>")
+              .append($("<button type='button' class='btn btn-primary'>")
+              .text("modifier").on("click",tasks[i],modifier))
+              .append($("<button type='button' class='btn btn-danger'>")
+              .text("supprimer").on("click",tasks[i],supprimer)));
+
+              // $("#liste_button")
+              // .append($("<div class='list'>")
+              // .append($("<button type='button' id='modifier' class='list-group-item'>")
+              // .text("modifier")).on("click",tasks[i],modifier));
           }
         }
 
-    function goTo(event){
-        $("#currenttask").empty();
-        // formTask();
-        // fillFormTask(event.data);
-        }
+    function goTo(event)
+    {
+      $("#currenttask").empty();
+      // formTask();
+      // fillFormTask(event.data);
+    }
+
+    function modifier(event)
+    {
+      alert("modifier");
+    }
+
+    function supprimer(event)
+    {
+      console.log("event : ");
+      let url = "http://localhost:3000/quizz/"+event.data.id;
+      let init ={
+        headers : {
+          Accept : 'application/json',
+          contentType: "application/json"
+        },
+        method : "DELETE"
+      }
+      fetch(url, init)
+      .then(response =>
+      {
+        console.log("DELETE Success");
+      })
+      .catch(err => {console.warn(err)});
+
+      refreshTaskList();
+      refreshTaskList();
+    }
 
 
     refreshTaskList();
